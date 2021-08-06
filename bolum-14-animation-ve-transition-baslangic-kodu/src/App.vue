590eb82969be20e081b7d6792388015b9cce5b81
Serdar EMIRCI <s@emirci.com>
2021-08-06 17:12:21 +0300

    bolum-14

--- bolum-14-animation-ve-transition-baslangic-kodu/src/App.vue
+++ bolum-14-animation-ve-transition-baslangic-kodu/src/App.vue
@@ -3,7 +3,13 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Animation ve Transition</h3>
-        <hr>
+        <hr />
+        <button class="btn btn-primary" @click="show = !show">Göster/Gizle</button>
+
+        <br /><br />
+        <transition>
+          <div class="alert alert-success" v-if="show">Bu alert kutusu</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -11,9 +17,12 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
