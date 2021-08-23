7d07538627762ca15526c4dffaaf1dfcc84357be
Serdar EMIRCI <serdar@emirci.com>
2021-08-24 00:15:20 +0300

    bolum-16-vue-resource-ile-http-servislerine-baglanma tamamlandı.

--- bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/App.vue
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/App.vue
@@ -9,7 +9,12 @@
           <button class="btn btn-primary" @click="getUsers">Get</button>
           <hr />
           <ul class="list-group">
-          <li class="list-group item" v-for="user in users" :key="user">{{user.userName}}</li>
+            <li class="list-group item" v-for="user in users" :key="user.id">
+              <span> {{ user.data.userName }}</span>
+              <button class="btn btn-xs btn-danger" @click="deleteUser(user.key)">
+                Sil
+              </button>
+            </li>
           </ul>
         </div>
       </div>
@@ -23,32 +28,52 @@ export default {
     return {
       userName: "",
       users: [],
+      resource: {},
     };
   },
   methods: {
+    deleteUser(key) {
+      // this.$http.delete("users/" + key + ".json").then((response) => {
+      //   console.log(response);
+      // });
+
+      this.$resource("users/" + key + ".json").delete();
+    },
     saveUser() {
-      this.$http
-        .post(
-          "https://gopdestek-c4e3e-default-rtdb.europe-west1.firebasedatabase.app/users.json",
-          { userName: this.userName }
-        )
-        .then((response) => {
-          console.log(response);
-        });
+      // this.$http.post("users.json", { userName: this.userName }).then((response) => {
+      //   console.log(response);
+      // });
+      // https://github.com/pagekit/vue-resource/blob/develop/docs/resource.md
+      // this.$resource("users.json").save({}, { userName: this.userName });
+      this.resource.kaydet({}, { userName: this.userName });
     },
     getUsers() {
-      this.$http
-        .get(
-          "https://gopdestek-c4e3e-default-rtdb.europe-west1.firebasedatabase.app/users.json"
-        )
+      this.$resource("users.json")
+        .get()
         .then((response) => {
-          let responseData = response.data;
-          for(let key in data){
-            this.users.push(data[key])
-          }
+          // let responseData = response.body;
+          // for (let key in responseData) {
+          //   this.users.push(responseData[key]);
+          // }
+          return response.json();
           console.log(response.body);
+        })
+        .then((data) => {
+          console.log(data);
+          for (let key in data.userList) {
+            this.users.push({
+              key: key,
+              data: data.userList[key],
+            });
+          }
         });
     },
+  },
+  created() {
+    const customActions = {
+      kaydet: { method: "POST", url: "users.json" },
+    };
+    this.resource = this.$resource("users.json", {}, customActions);
   },
 };
 </script>
--- bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/main.js
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/main.js
@@ -4,6 +4,26 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = "https://gopdestek-c4e3e-default-rtdb.europe-west1.firebasedatabase.app";
+
+Vue.http.interceptors.push((request, next) => {
+    console.log("interceptors :" + request.method);
+    // if(request.method == "POST"){
+    //     request.method = "PUT"
+    // }
+    // next;
+
+    next(response => {
+        console.log(response);
+        response.json = () => {
+            return {
+                userList: response.body
+            }
+        }
+    });
+
+});
+
 new Vue({
     el: '#app',
     render: h => h(App)
